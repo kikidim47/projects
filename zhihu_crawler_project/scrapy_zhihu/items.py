e3e7c6c55b663c2314c6ec4e4ec8b1e02f4dfd15
kikidim47 <166269948+kikidim47@users.noreply.github.com>
2025-07-30 17:11:17 +0800

    delete all

--- zhihu_crawler_project/scrapy_zhihu/items.py
+++
@@ -1,8 +0,0 @@
-import scrapy
-
-class ZhihuItem(scrapy.Item):
-    title = scrapy.Field()
-    url = scrapy.Field()
-    answer_author = scrapy.Field()
-    answer_content = scrapy.Field()
-    vote_count = scrapy.Field()
--- zhihu_crawler_project/scrapy_zhihu/settings.py
+++
@@ -1,8 +0,0 @@
-ROBOTSTXT_OBEY = False
-USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)...'
-DOWNLOADER_MIDDLEWARES = {
-    'scrapy_zhihu.middlewares.ProxyMiddleware': 543,
-}
-ITEM_PIPELINES = {
-    'scrapy_zhihu.pipelines.MongoDBPipeline': 300,
-}
